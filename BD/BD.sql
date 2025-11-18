b52de91aee0f1a5c9565a7cc5dcc38cafbbf36f8
tobiane <tobiasinzunza@gmail.com>
2025-11-18 15:55:31 -0300

    me habia olvidado

--- BD/BD.sql
+++ BD/BD.sql
@@ -221,6 +221,32 @@ INSERT INTO `departamentos` VALUES (1,'Direccion',85000.00),(2,'RRHH',120000.00)
 UNLOCK TABLES;
 
 --
+-- Table structure for table `empleados`
+--
+
+DROP TABLE IF EXISTS `empleados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empleados` (
+  `ID_Empleado` int(8) NOT NULL,
+  `Nombre` varchar(50) DEFAULT NULL,
+  `Apellido` varchar(50) DEFAULT NULL,
+  `Fecha_Contratacion` date DEFAULT NULL,
+  `ID_Departamento` int(8) NOT NULL,
+  PRIMARY KEY (`ID_Empleado`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empleados`
+--
+
+LOCK TABLES `empleados` WRITE;
+/*!40000 ALTER TABLE `empleados` DISABLE KEYS */;
+/*!40000 ALTER TABLE `empleados` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `empleados_rrhh`
 --
 
@@ -231,17 +257,15 @@ CREATE TABLE `empleados_rrhh` (
   `ID_Empleado` int(8) NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(50) DEFAULT NULL,
   `Apellido` varchar(50) DEFAULT NULL,
+  `Departamento` varchar(50) DEFAULT NULL,
   `Fecha_Nacimiento` date DEFAULT NULL,
   `Direccion` varchar(100) DEFAULT NULL,
   `Telefono` varchar(15) DEFAULT NULL,
   `Email` varchar(100) DEFAULT NULL,
   `Fecha_Contratacion` date DEFAULT NULL,
   `Puesto` varchar(50) DEFAULT NULL,
-  `ID_Departamento` int(8) unsigned DEFAULT NULL,
-  PRIMARY KEY (`ID_Empleado`),
-  KEY `fk_empleado_departamento` (`ID_Departamento`),
-  CONSTRAINT `fk_empleado_departamento` FOREIGN KEY (`ID_Departamento`) REFERENCES `departamentos` (`ID_departamento`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  PRIMARY KEY (`ID_Empleado`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -250,7 +274,6 @@ CREATE TABLE `empleados_rrhh` (
 
 LOCK TABLES `empleados_rrhh` WRITE;
 /*!40000 ALTER TABLE `empleados_rrhh` DISABLE KEYS */;
-INSERT INTO `empleados_rrhh` VALUES (1,'asd','asd','2025-11-18','asd','a34','afaef','2025-11-05','asd',3);
 /*!40000 ALTER TABLE `empleados_rrhh` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -364,7 +387,7 @@ CREATE TABLE `maquinas` (
 
 LOCK TABLES `maquinas` WRITE;
 /*!40000 ALTER TABLE `maquinas` DISABLE KEYS */;
-INSERT INTO `maquinas` VALUES (1,'Cinta de correr','Activo',2),(2,'Bicicleta fija','Inactivo',2),(3,'Elíptica','Activo',2),(4,'Máquina de pecho','Activo',2),(5,'Máquina de piernas','Inactivo',2),(6,'Cinta de correr','Activa',3),(7,'Bicicleta fija','Activa',3),(8,'Elíptica','Activa',3),(9,'Máquina de pecho','Activa',3),(10,'Máquina de piernas','Activa',3),(11,'Cinta de correr','Activa',4),(12,'Bicicleta fija','Activa',4),(13,'Elíptica','Activa',4),(14,'Máquina de pecho','Activa',4),(15,'Máquina de piernas','Activa',4),(16,'Cinta de correr','Activa',5),(17,'Bicicleta fija','Activa',5),(18,'Elíptica','Activa',5),(19,'Máquina de pecho','Activa',5),(20,'Máquina de piernas','Activa',5),(21,'Cinta de correr','Activa',6),(22,'Bicicleta fija','Activa',6),(23,'Elíptica','Activa',6),(24,'Máquina de pecho','Activa',6),(25,'Máquina de piernas','Activa',6),(26,'Cinta de correr','Activa',7),(27,'Bicicleta fija','Activa',7),(28,'Elíptica','Activa',7),(29,'Máquina de pecho','Activa',7),(30,'Máquina de piernas','Activa',7),(31,'Cinta de correr','Activa',8),(32,'Bicicleta fija','Activa',8),(33,'Elíptica','Activa',8),(34,'Máquina de pecho','Activa',8),(35,'Máquina de piernas','Activa',8),(36,'Cinta de correr','Activa',9),(37,'Bicicleta fija','Activa',9),(38,'Elíptica','Activa',9),(39,'Máquina de pecho','Activa',9),(40,'Máquina de piernas','Activa',9),(41,'Cinta de correr','Activa',10),(42,'Bicicleta fija','Activa',10),(43,'Elíptica','Activa',10),(44,'Máquina de pecho','Activa',10),(45,'Máquina de piernas','Activa',10),(46,'Cinta de correr','Activa',11),(47,'Bicicleta fija','Activa',11),(48,'Elíptica','Activa',11),(49,'Máquina de pecho','Activa',11),(50,'Máquina de piernas','Activa',11),(51,'Cinta de correr','Activa',12),(52,'Bicicleta fija','Activa',12),(53,'Elíptica','Activa',12),(54,'Máquina de pecho','Activa',12),(55,'Máquina de piernas','Activa',12),(56,'Cinta de correr','Activa',13),(57,'Bicicleta fija','Activa',13),(58,'Elíptica','Activa',13),(59,'Máquina de pecho','Activa',13),(60,'Máquina de piernas','Activa',13),(61,'Cinta de correr','Activa',14),(62,'Bicicleta fija','Activa',14),(63,'Elíptica','Activa',14),(64,'Máquina de pecho','Activa',14),(65,'Máquina de piernas','Activa',14),(66,'Cinta de correr','Activa',15),(67,'Bicicleta fija','Activa',15),(68,'Elíptica','Activa',15),(69,'Máquina de pecho','Activa',15),(70,'Máquina de piernas','Activa',15),(71,'Cinta de correr','Activa',16),(72,'Bicicleta fija','Activa',16),(73,'Elíptica','Activa',16),(74,'Máquina de pecho','Activa',16),(75,'Máquina de piernas','Activa',16),(76,'Cinta de correr','Activa',17),(77,'Bicicleta fija','Activa',17),(78,'Elíptica','Activa',17),(79,'Máquina de pecho','Activa',17),(80,'Máquina de piernas','Activa',17),(81,'cinta de correr','Activa',18),(82,'cinta de correr','Activa',18),(83,'Jalon al pecho','Activa',18),(84,'Elíptica','Activa',18),(85,'Press banca','Activa',18);
+INSERT INTO `maquinas` VALUES (1,'Cinta de correr','Inactivo',2),(2,'Bicicleta fija','Activo',2),(3,'Elíptica','Inactivo',2),(4,'Máquina de pecho','Inactivo',2),(5,'Máquina de piernas','Inactivo',2),(6,'Cinta de correr','Activa',3),(7,'Bicicleta fija','Activa',3),(8,'Elíptica','Activa',3),(9,'Máquina de pecho','Activa',3),(10,'Máquina de piernas','Activa',3),(11,'Cinta de correr','Activa',4),(12,'Bicicleta fija','Activa',4),(13,'Elíptica','Activa',4),(14,'Máquina de pecho','Activa',4),(15,'Máquina de piernas','Activa',4),(16,'Cinta de correr','Activa',5),(17,'Bicicleta fija','Activa',5),(18,'Elíptica','Activa',5),(19,'Máquina de pecho','Activa',5),(20,'Máquina de piernas','Activa',5),(21,'Cinta de correr','Activa',6),(22,'Bicicleta fija','Activa',6),(23,'Elíptica','Activa',6),(24,'Máquina de pecho','Activa',6),(25,'Máquina de piernas','Activa',6),(26,'Cinta de correr','Activa',7),(27,'Bicicleta fija','Activa',7),(28,'Elíptica','Activa',7),(29,'Máquina de pecho','Activa',7),(30,'Máquina de piernas','Activa',7),(31,'Cinta de correr','Activa',8),(32,'Bicicleta fija','Activa',8),(33,'Elíptica','Activa',8),(34,'Máquina de pecho','Activa',8),(35,'Máquina de piernas','Activa',8),(36,'Cinta de correr','Activa',9),(37,'Bicicleta fija','Activa',9),(38,'Elíptica','Activa',9),(39,'Máquina de pecho','Activa',9),(40,'Máquina de piernas','Activa',9),(41,'Cinta de correr','Activa',10),(42,'Bicicleta fija','Activa',10),(43,'Elíptica','Activa',10),(44,'Máquina de pecho','Activa',10),(45,'Máquina de piernas','Activa',10),(46,'Cinta de correr','Activa',11),(47,'Bicicleta fija','Activa',11),(48,'Elíptica','Activa',11),(49,'Máquina de pecho','Activa',11),(50,'Máquina de piernas','Activa',11),(51,'Cinta de correr','Activa',12),(52,'Bicicleta fija','Activa',12),(53,'Elíptica','Activa',12),(54,'Máquina de pecho','Activa',12),(55,'Máquina de piernas','Activa',12),(56,'Cinta de correr','Activa',13),(57,'Bicicleta fija','Activa',13),(58,'Elíptica','Activa',13),(59,'Máquina de pecho','Activa',13),(60,'Máquina de piernas','Activa',13),(61,'Cinta de correr','Activa',14),(62,'Bicicleta fija','Activa',14),(63,'Elíptica','Activa',14),(64,'Máquina de pecho','Activa',14),(65,'Máquina de piernas','Activa',14),(66,'Cinta de correr','Activa',15),(67,'Bicicleta fija','Activa',15),(68,'Elíptica','Activa',15),(69,'Máquina de pecho','Activa',15),(70,'Máquina de piernas','Activa',15),(71,'Cinta de correr','Activa',16),(72,'Bicicleta fija','Activa',16),(73,'Elíptica','Activa',16),(74,'Máquina de pecho','Activa',16),(75,'Máquina de piernas','Activa',16),(76,'Cinta de correr','Activa',17),(77,'Bicicleta fija','Activa',17),(78,'Elíptica','Activa',17),(79,'Máquina de pecho','Activa',17),(80,'Máquina de piernas','Activa',17),(81,'cinta de correr','Activa',18),(82,'cinta de correr','Activa',18),(83,'Jalon al pecho','Activa',18),(84,'Elíptica','Activa',18),(85,'Press banca','Activa',18);
 /*!40000 ALTER TABLE `maquinas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -675,7 +698,7 @@ CREATE TABLE `usuario` (
   UNIQUE KEY `IdUsuario` (`idUsuario`),
   KEY `id_departamento` (`id_departamento`),
   CONSTRAINT `fk_departamento` FOREIGN KEY (`id_departamento`) REFERENCES `departamentos` (`ID_departamento`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -684,7 +707,6 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'tobi','inzunza','tobi@gmail.com','cd6f33ab869d39460147064d5c5a72f182f7bb9502ed73f8e239a5a62ff69d42','2025-11-04','Empleado',0,2),(3,'luchi','luchi','luchi@gmail.com','41e907a49caf28996f7c9ac20dc0be626d19664cfd09f77e8494fcf4c34107e4','2025-11-04','Empleado',0,2),(4,'tobi','tobi','tobias@gmail.com','cd6f33ab869d39460147064d5c5a72f182f7bb9502ed73f8e239a5a62ff69d42','2025-11-04','Empleado',1,2);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -697,4 +719,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-11-06 17:37:50
+-- Dump completed on 2025-11-18 15:54:29
